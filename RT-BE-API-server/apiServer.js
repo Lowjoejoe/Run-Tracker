c6ddbfc998455b501502a12c348c4735bf5f59b2
[Joseph <[joseph.t.low@gmail.com]>
2022-10-12 11:32:45 -0700

    initial commit of backend api server and database

---
+++ RT-BE-API-server/apiServer.js
@@ -0,0 +1,156 @@
+//set dependencies 
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const {Client} = require('pg');
+const config = require ('./config.js')[process.env.NODE_ENV || "dev"]
+
+const PORT = config.port 
+
+const client = new Client ({
+    connectionString: config.connectionString,
+})
+
+client.connect();
+
+//set up middleware 
+app.use(cors());
+app.use(express.json());
+
+//set up routes 
+
+//initial blank test route 
+app.get('/',(req,res)=>{
+    res.send("Welcome to RunTracker.com")
+})
+
+//route to return all runners
+app.get('/api/runners',(req,res,err)=>{
+    client.query('SELECT * FROM runners')
+    .then(results=>{
+        res.status(200);
+        res.send(results.rows);
+    })
+    .catch((err)=>console.log("error"));
+});
+
+//route to return all workouts 
+app.get('/api/workouts',(req,res,err)=>{
+    client.query('SELECT * FROM workouts')
+    .then(results=>{
+        res.status(200);
+        res.send(results.rows);
+    })
+    .catch((err)=>console.log('error'));
+});
+
+//route to return inner join of runners and workouts 
+app.get('/api/runners-workouts',(req,res,err)=>{
+    client.query('SELECT name, distance, time, location, type FROM workouts INNER JOIN runners ON workouts.runner_id = runners.runner_id')
+    .then(results=>{
+        res.status(200);
+        res.send(results.rows);
+    })
+    .catch((err)=>console.log('error'));
+});
+
+//route to get runners by ID
+app.get('/api/runners/:id', (req,res,err)=>{
+    client.query(`SELECT * FROM runners WHERE runner_id = ${req.params.id}`)
+    .then(results=>{
+        if(results.rows.length == 0){
+            res.status(404);
+            res.send(`Runner ID not found in database`);
+            return;
+        }
+        res.status(200);
+        res.send(results.rows)
+    })
+    .catch((err)=>console.log('error'))
+});
+
+//route to get workouts by ID 
+app.get('/api/workouts/:id', (req,res,err)=>{
+    client.query(`SELECT * FROM workouts WHERE workout_id = ${req.params.id}`)
+    .then(results=>{
+        if(results.rows.length == 0){
+            res.status(404);
+            res.send(`Workout ID not found in database`);
+            return;
+        }
+        res.status(200);
+        res.send(results.rows)
+    })
+    .catch((err)=>console.log('error'))
+});
+
+//route to get runners workouts by runner name
+app.get('/api/runners-workouts/name/:name', (req,res,err)=>{
+    client.query(`SELECT name, distance, time, location, type FROM workouts INNER JOIN runners ON workouts.runner_id = runners.runner_id WHERE name = '${req.params.name}'`)
+    .then(results=>{
+        if(results.rows.length == 0){
+        res.status(404); 
+        res.send(`Runner's name not found in database`);
+        return;
+        }
+    res.status(200);
+    res.send(results.rows);
+    })
+    .catch((err)=>console.log('error'));
+});
+
+//route to delete workout from database by id 
+app.delete('/api/workouts/:id', (req,res)=>{
+    client.query(`SELECT * FROM workouts WHERE workout_id = ${req.params.id}`)
+    .then(results=>{
+        if (results.rows.length == 0){
+            res.status(404);
+            res.send(`workout doesn't exist in the database`);
+            return;
+        }else{
+            let deletedWorkout = JSON.stringify(results.rows);
+            res.status(200);
+            res.send(`Workout data deleted from database ${deletedWorkout}`);
+            client.query(`DELETE FROM workouts WHERE id = ${req.params.id}`);
+        }
+    })
+});
+
+//route to delete runner from database by id 
+app.delete('/api/runners/:id', (req,res)=>{
+    client.query(`SELECT * FROM runners WHERE runner_id = ${req.params.id}`)
+    .then(results=>{
+        if (results.rows.length == 0){
+            res.status(404);
+            res.send(`Runner doesn't exist in the database`);
+            return;
+        }else{
+            let deletedWorkout = JSON.stringify(results.rows);
+            res.status(200);
+            res.send(`Runner data deleted from database ${deletedWorkout}`);
+            client.query(`DELETE FROM runners WHERE id = ${req.params.id}`);
+        }
+    })
+});
+
+//route to patch workout in database by id 
+app.patch('/api/workouts/:id',(req,res)=>{
+    let workoutUpdate = req.body;
+    if(workoutUpdate.distance && workoutUpdate.time && workoutUpdate.location && workoutUpdate.type && typeof workoutUpdate.distance == 'number'&& workoutUpdate.distance.length !=0 && workoutUpdate.time.length !=0 && workoutUpdate.location.length !=0 && workoutUpdate.type.length !=0){
+        client.query(`UPDATE workouts SET distance = ${workoutUpdate.distance}, time = '${workoutUpdate.time}', location = '${workoutUpdate.location}', type = '${workoutUpdate.type}'WHERE workout_id = ${req.params.id}`)
+        .then(results=>{
+            res.status(200);
+            let workoutUpdateString = JSON.stringify(workoutUpdate);
+            res.send(`Workout at ID: ${req.params.id} updated to: ${workoutUpdateString}`);
+        })
+    }else{
+        res.status(404);
+        res.send(`404 ERROR: Bad patch request please provide workout: distance as number |time|location|type`);
+    }
+});
+
+
+//set up listen on port
+app.listen(PORT,()=>{
+    console.log(`server is running on PORT: ${PORT}`);
+});
